53070484877c3b63da323e009c49edd0a2fcaead
Moogle2346 <Tnkd1422>
2021-09-21 00:59:40 +0900

    プロジェクト ファイルを追加します。

---
+++ EncryptionTest/CryptionClass.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+using System.IO;
+
+namespace EncryptionTest
+{
+    class CryptionClass
+    {
+        /// <summary>
+        /// 文字列をAESで暗号化
+        /// </summary>
+        /// <param name="text">暗号化したい文字列</param>
+        /// <returns>Base64形式で暗号化された文字列</returns>
+        public string Encrypt(string text)
+        {
+            // 初期化ベクトル            ----- (4)
+            byte[] AesIV = {0x00, 0x10, 0x20, 0x30, 0x40, 0x50, 0x60, 0x70, 0x80, 0x90, 0xA0, 0xB0, 0xC0, 0xD0, 0xE0, 0xF0 };
+            // 暗号化鍵
+            byte[] AesKey = {0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F };
+
+            // 暗号化方式はAES           ----- (1)
+            AesManaged aes = new AesManaged();
+            // 鍵の長さ                  ----- (2)
+            aes.KeySize = 128;
+            // ブロックサイズ（何文字単位で処理するか）
+            aes.BlockSize = 128;
+            // 暗号利用モード             ----- (3)
+            aes.Mode = CipherMode.ECB;
+            aes.IV = AesIV;
+            aes.Key = AesKey;
+            // パディング                 ----- (5)
+            aes.Padding = PaddingMode.PKCS7;
+
+            // 暗号化するためにはバイトの配列に変換する必要がある
+            byte[] byteText = Encoding.UTF8.GetBytes(text);
+
+            // 暗号化
+            byte[] encryptText = aes.CreateEncryptor().TransformFinalBlock(byteText, 0, byteText.Length);
+
+            // Base64形式（64種類の英数字で表現）で返す
+            return Convert.ToBase64String(encryptText);
+        }
+    }
+}
---
+++ EncryptionTest/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿
+namespace EncryptionTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtB_PlainText = new System.Windows.Forms.TextBox();
+            this.txtB_CipherText = new System.Windows.Forms.TextBox();
+            this.btn_Encrypt = new System.Windows.Forms.Button();
+            this.btn_Decrypt = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtB_PlainText
+            // 
+            this.txtB_PlainText.Location = new System.Drawing.Point(51, 49);
+            this.txtB_PlainText.Multiline = true;
+            this.txtB_PlainText.Name = "txtB_PlainText";
+            this.txtB_PlainText.Size = new System.Drawing.Size(292, 346);
+            this.txtB_PlainText.TabIndex = 0;
+            // 
+            // txtB_CipherText
+            // 
+            this.txtB_CipherText.Location = new System.Drawing.Point(479, 49);
+            this.txtB_CipherText.Multiline = true;
+            this.txtB_CipherText.Name = "txtB_CipherText";
+            this.txtB_CipherText.Size = new System.Drawing.Size(292, 346);
+            this.txtB_CipherText.TabIndex = 1;
+            // 
+            // btn_Encrypt
+            // 
+            this.btn_Encrypt.Location = new System.Drawing.Point(368, 101);
+            this.btn_Encrypt.Name = "btn_Encrypt";
+            this.btn_Encrypt.Size = new System.Drawing.Size(86, 43);
+            this.btn_Encrypt.TabIndex = 2;
+            this.btn_Encrypt.Text = "暗号化";
+            this.btn_Encrypt.UseVisualStyleBackColor = true;
+            this.btn_Encrypt.Click += new System.EventHandler(this.btn_Encrypt_Click);
+            // 
+            // btn_Decrypt
+            // 
+            this.btn_Decrypt.Location = new System.Drawing.Point(368, 256);
+            this.btn_Decrypt.Name = "btn_Decrypt";
+            this.btn_Decrypt.Size = new System.Drawing.Size(86, 43);
+            this.btn_Decrypt.TabIndex = 3;
+            this.btn_Decrypt.Text = "復号";
+            this.btn_Decrypt.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn_Decrypt);
+            this.Controls.Add(this.btn_Encrypt);
+            this.Controls.Add(this.txtB_CipherText);
+            this.Controls.Add(this.txtB_PlainText);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtB_PlainText;
+        private System.Windows.Forms.TextBox txtB_CipherText;
+        private System.Windows.Forms.Button btn_Encrypt;
+        private System.Windows.Forms.Button btn_Decrypt;
+    }
+}
+
